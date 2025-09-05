8c87c70faaa1b813c152f9c71183d53517cf5f48
majid <majid.gha3mi.p@gmail.com>
2025-09-05 23:06:22 +0330

    glass_botton created

---
+++ lib/feature/home/widget/glass_button.dart
@@ -0,0 +1,51 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class GlassButton extends StatelessWidget {
+  final String text;
+  final double widthFactor;
+  final VoidCallback onTap;
+
+  const GlassButton({
+    super.key,
+    required this.text,
+    required this.widthFactor,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
+        child: InkWell(
+          onTap: onTap,
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          child: Container(
+            height: size.width / 8,
+            width: size.width / widthFactor,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              // ignore: deprecated_member_use
+              color: Colors.white.withOpacity(.05),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Text(
+              text,
+
+              style: TextStyle(
+                // ignore: deprecated_member_use
+                color: Colors.white.withOpacity(.8),
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
