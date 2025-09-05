13c269dd6c03603cfe3834b338bcbaa3e9074ecb
majid <majid.gha3mi.p@gmail.com>
2025-09-05 23:17:37 +0330

    buildBackground created

--- lib/feature/home/view/home_page.dart
+++ lib/feature/home/view/home_page.dart
@@ -35,40 +35,7 @@ class HomePage extends GetView<HomeController> {
     );
   }
 
-  Widget _buildBackground(Size size) {
-    return Stack(
-      children: [
-        Positioned(
-          top: size.height * (controller.anim2.value + .58),
-          left: size.width * .21,
-          child: const CustomPaint(painter: CirclePainter(50)),
-        ),
-        Positioned(
-          top: size.height * .98,
-          left: size.width * .1,
-          child: CustomPaint(
-            painter: CirclePainter(controller.anim4.value - 30),
-          ),
-        ),
-        Positioned(
-          top: size.height * .5,
-          left: size.width * (controller.anim2.value + .8),
-          child: const CustomPaint(painter: CirclePainter(30)),
-        ),
-        Positioned(
-          top: size.height * controller.anim3.value,
-          left: size.width * (controller.anim1.value + .1),
-          child: const CustomPaint(painter: CirclePainter(60)),
-        ),
-        Positioned(
-          top: size.height * .1,
-          left: size.width * .8,
-          child: CustomPaint(painter: CirclePainter(controller.anim4.value)),
-        ),
-      ],
-    );
-  }
-
+  
   Widget _buildContent(
     //Size size
   ) {
